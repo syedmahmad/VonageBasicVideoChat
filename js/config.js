428d0f906f69cea4d845f2af3749a1222b4928f6
syedmahmad <devm.ahmad@gmail.com>
2024-06-28 17:34:49 +0500

    Basic Vidoe api setup

---
+++ js/config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// Make a copy of this file and save it as config.js (in the js directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+const SAMPLE_SERVER_BASE_URL = 'https://YOUR-SERVER-URL';
+
+// OR, if you have not set up a web server that runs the sample-video-php-learning_server code,
+// set these values to Vonage Application ID, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tools.vonage.com/video/playground.
+
+const APPLICATION_ID = '47765381';
+const SESSION_ID = '1_MX40Nzc2NTM4MX4xNjEuMjI2LjEzNC4xNDB-MTY5MzA3NDg5Nzk3Nn4yY0JkazNqbnorQmw3TERqbDJ1ckwvU0h-fn4';
+const TOKEN = 'T1==cGFydG5lcl9pZD00Nzc2NTM4MSZzaWc9ZTVlZWM0NjJkODJhYzI1YTk0NzgxZDdjY2RjNDZiMWYwOWUwMmFhMzpzZXNzaW9uX2lkPTFfTVg0ME56YzJOVE00TVg0eE5qRXVNakkyTGpFek5DNHhOREItTVRZNU16QTNORGc1TnprM05uNHlZMEprYXpOcWJub3JRbXczVEVScWJESjFja3d2VTBoLWZuNCZjcmVhdGVfdGltZT0xNzE5NTc1MzQzJm5vbmNlPTAuOTE0MDE4ODUyNjA2OTY3JnJvbGU9bW9kZXJhdG9yJmV4cGlyZV90aW1lPTE3MTk1ODg4OTAmY29ubmVjdGlvbl9kYXRhPTEzOTA5JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
